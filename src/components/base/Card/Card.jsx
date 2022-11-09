3b7e3ce3fb97f3a38b1ded33370e7308efa1d2f4
Luis Villacorta <lvillacorta@itereo.travel>
2022-11-09 01:08:14 -0500

    feat:adjusts card width and scrollbar

--- src/components/base/Card/Card.jsx
+++ src/components/base/Card/Card.jsx
@@ -8,11 +8,16 @@ export const Card = ({ product = {} }) => {
                 <img src="src/assets/pizza.png" alt="" />
             </picture>
             <p className={style.card__name}>{product.nombre}</p>
-            <p className={style.card__precio}>S/ {product.precio}</p>
-            <p className={style.card__precioFinal}>S/ {product.precioFinal}</p>
+            <p className={style.card__description}>{product.descripcion}</p>
             <div className={style.card__footer}>
-                <button className={style.card__btnCart}><Cart /></button>
-                <button className={style.card__btnComprar}>Comprar</button>
+                <div>
+                    <p className={style.card__precio}>S/ {product.precio}</p>
+                    <p className={style.card__precioFinal}>S/ {product.precioFinal}</p>
+                </div>
+                <div className={style.btn__container}>
+                    <button className={style.card__btnCart}><Cart /></button>
+                    <button className={style.card__btnComprar}>Comprar</button>
+                </div>
             </div>
             {/* <PizzaBg qty={2}/> */}
         </div>  
--- src/mock-data/Products.js
+++ src/mock-data/Products.js
@@ -2,48 +2,56 @@ export const products = [
     {
         id: 0,
         nombre: "Pizzaa Americana",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis. Duis rutrum pretium sapien a laoreet. Sed nec lobortis tortor. Duis mollis rutrum metus, sed vehicula ante pellentesque ac.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 1,
         nombre: "Pizzaa Americana 1",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 2,
         nombre: "Pizzaa Americana 2",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 3,
         nombre: "Pizzaa Americana 3",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 0,
         nombre: "Pizzaa Americana",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 1,
         nombre: "Pizzaa Americana 1",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 2,
         nombre: "Pizzaa Americana 2",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 3,
         nombre: "Pizzaa Americana 3",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
         precio: 40.00,
         precioFinal: 30.00
     }
