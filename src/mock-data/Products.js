6df7000cfb6a4ddbef4d43a006f459cc5978f2a2
Luis Fernando Villacorta Loarte <85130000+luisvilla8@users.noreply.github.com>
2022-11-14 11:23:29 -0500

    feat: changes products mock-data

--- src/mock-data/Products.js
+++ src/mock-data/Products.js
@@ -1,58 +1,58 @@
 export const products = [
     {
         id: 0,
-        nombre: "Pizzaa Americana",
+        nombre: "Pizza Americana - Familiar",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis. Duis rutrum pretium sapien a laoreet. Sed nec lobortis tortor. Duis mollis rutrum metus, sed vehicula ante pellentesque ac.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 1,
-        nombre: "Pizzaa Americana 1",
+        nombre: "Pizzaa Hawaiana - Familiar",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
-        precio: 40.00,
-        precioFinal: 30.00
+        precio: 45.00,
+        precioFinal: 34.00
     },
     {
         id: 2,
-        nombre: "Pizzaa Americana 2",
+        nombre: "Pizzaa Napolitana - Familiar",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
-        precio: 40.00,
-        precioFinal: 30.00
+        precio: 42.00,
+        precioFinal: 32.00
     },
     {
         id: 3,
-        nombre: "Pizzaa Americana 3",
+        nombre: "Pizzaa de Peperoni - Familiar",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
         precio: 40.00,
         precioFinal: 30.00
     },
     {
         id: 4,
-        nombre: "Pizzaa Americana",
+        nombre: "Pizzaa Americana - Personal",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
-        precio: 40.00,
-        precioFinal: 30.00
+        precio: 16.00,
+        precioFinal: 15.00
     },
     {
         id: 5,
-        nombre: "Pizzaa Americana 1",
+        nombre: "Pizzaa Hawaiana - Personal",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
-        precio: 40.00,
-        precioFinal: 30.00
+        precio: 18.00,
+        precioFinal: 17.00
     },
     {
         id: 6,
-        nombre: "Pizzaa Americana 2",
+        nombre: "Pizzaa Napolitana - Personal",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
-        precio: 40.00,
-        precioFinal: 30.00
+        precio: 20.00,
+        precioFinal: 18.00
     },
     {
         id: 7,
-        nombre: "Pizzaa Americana 3",
+        nombre: "Pizzaa de Peperoni - Personal",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis risus dui, in varius justo fermentum eu. Pellentesque tincidunt nibh turpis.",
-        precio: 40.00,
-        precioFinal: 30.00
+        precio: 20.00,
+        precioFinal: 18.00
     }
 ];
